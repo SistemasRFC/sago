97403041dd89953f46aac517454d0371b97c4b0a
C1299844 Rafael Freitas Carneiro <c1299844@interno.bb.com.br>
2021-03-22 09:28:29 -0300

    123456

--- Model/RelatorioGerencial/RelatorioGerencialModel.php
+++ Model/RelatorioGerencial/RelatorioGerencialModel.php
@@ -63,8 +63,8 @@ class RelatorioGerencialModel extends BaseModel{
                         if ($lista[0]){
                             $nomeUsuario = str_replace(" ", "", $listaOfs[1][$i]['NME_USUARIO_COMPLETO']);
                             $nomeArquivo='projeto_'.$this->objRequest->nroMesReferencia.$this->objRequest->nroAnoReferencia.'_'.$nomeUsuario.'_'.$listaOfs[1][$i]['COD_OF'].'.txt';
-//                            ini_set('display_errors', true);
-//                            error_reporting(E_ALL);
+                            ini_set('display_errors', true);
+                            error_reporting(E_ALL);
                             $nomeArquivo = str_replace('(', '', $nomeArquivo);
                             $nomeArquivo = str_replace(')', '', $nomeArquivo);
                             $arquivo = fopen($pasta.$nomeArquivo,'w');
